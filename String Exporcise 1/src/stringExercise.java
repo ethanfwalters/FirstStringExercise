3b59903d8474aa3cf1f60a039b3d685f1e2e61b0
ethanwalters <ethanwalters@192.168.0.39>
2017-09-10 12:23:20 -0600

    initial commit

---
+++ String Exporcise 1/src/stringExercise.java
@@ -0,0 +1,82 @@
+
+public class stringExercise
+	{
+
+		public static void main(String[] args)
+			{
+				//challenge one
+				
+				String alphabet = "a b c d e f g h i j k l m n o p q r s t u v w x y z" ;
+				
+				int aLength = alphabet.length();
+				
+				System.out.println();
+				
+				for( int i = 0 ; i  < aLength ; i = i + 2){
+					
+					String letter = alphabet.substring(i , i + 1);
+					System.out.print(letter);
+				}
+				
+				System.out.println();
+				
+				//challenge two
+				
+				int bLength = alphabet.length();
+				
+				for( int i = 0 ; i  < bLength ; i = i + 4){
+					String letter = alphabet.substring(i, i + 1);
+					System.out.print(letter + " ");
+				}
+				
+				
+				
+				//challenge three
+				
+				/*
+				int cLength = alphabet.length();
+				
+				for( int i = 0 ; i  < cLength ; i = i + 4){
+					
+					int o = i++;
+					int p = i + 2;
+					
+					String letter = alphabet.substring(o , p);
+					System.out.print(letter + " ");
+				}
+				*/
+				
+				// challenge four
+				
+				int dLength = alphabet.length();
+				
+				for( int i = 0 ; i  < dLength ; i = i + 2){
+					String letter = alphabet.substring(i, i + 1);
+					System.out.println(letter);
+				} 
+				
+				// challenge five
+				
+				int eLength = alphabet.length();
+				
+				System.out.println("Total number of charactures: " + eLength);
+				
+				// challenge six
+				
+				int fLength = alphabet.length();
+				
+				int numberOfSpaces = 0;
+				
+				for( int i = 0 ; i  < fLength ; i = i++){
+					String letter = alphabet.substring(i, i++);
+					if(letter.equals(" ")){
+						numberOfSpaces++;
+					}
+				}
+				
+				System.out.println(numberOfSpaces);
+
+
+			}
+
+	}
